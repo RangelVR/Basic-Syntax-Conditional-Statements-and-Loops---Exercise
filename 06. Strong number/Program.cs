9d393e26f9c8eda52af6d48a44e934c20cc1ef35
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-01-15 19:02:33 +0200

    Add files via upload

---
+++ 06. Strong number/Program.cs
@@ -0,0 +1,27 @@
+﻿string num = Console.ReadLine();
+int sum = 0;
+
+
+for (int i = 0; i < num.Length; i++)
+{
+    int n = (int)num[i] - '0';
+    int factorial = 1;
+
+    while (n > 1)
+	{
+		factorial *= n;
+		n--;
+	}
+	sum += factorial;
+}
+
+int inputNum = int.Parse(num);
+
+if (inputNum == sum)
+{
+	Console.WriteLine("yes");
+}
+else
+{
+	Console.WriteLine("no");
+}
