b9afbb3c5bd9a710ad3fd00a16702eaa40417f36
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-01-15 18:55:50 +0200

    Add files via upload

---
+++ 01. Ages/Program.cs
@@ -0,0 +1,12 @@
+﻿int age = int.Parse(Console.ReadLine());
+string output = string.Empty;
+
+switch (age)
+{
+    case <= 2: output = "baby"; break;
+    case <= 13: output = "child"; break;
+    case <= 19: output = "teenager"; break;
+    case <= 65: output = "adult"; break;
+    case > 65: output = "elder"; break;
+}
+Console.WriteLine(output);
