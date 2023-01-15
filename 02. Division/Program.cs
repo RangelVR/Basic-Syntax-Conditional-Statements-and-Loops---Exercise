65a6f56c267d3309368d84df41442a9bf4427db6
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-01-15 18:57:01 +0200

    Add files via upload

---
+++ 02. Division/Program.cs
@@ -0,0 +1,32 @@
+﻿int n = int.Parse(Console.ReadLine());
+int output = 0;
+
+if (n % 10 == 0)
+{
+    output = 10;
+}
+else if (n % 7 == 0)
+{
+    output = 7;
+}
+else if (n % 6 == 0)
+{
+    output = 6;
+}
+else if (n % 3 == 0)
+{
+    output = 3;
+}
+else if (n % 2 == 0)
+{
+    output = 2;
+}
+
+if (output != 0)
+{
+    Console.WriteLine($"The number is divisible by {output}");
+}
+else
+{
+    Console.WriteLine("Not divisible");
+}
