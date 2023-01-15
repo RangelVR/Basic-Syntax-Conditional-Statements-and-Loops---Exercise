2a2130ce69bb9783b60cbd9240e6b89819534b2e
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-01-15 19:01:10 +0200

    Add files via upload

---
+++ 05. Login/Program.cs
@@ -0,0 +1,33 @@
+﻿
+string userName = Console.ReadLine();
+string password = string.Empty;
+
+for (int i = userName.Length - 1; i >= 0; i--)
+{
+    password += userName[i];
+}
+
+int atempts = 4;
+
+while (atempts > 0)
+{
+    string inputPass = Console.ReadLine();
+    if (inputPass == password)
+    {
+        Console.WriteLine($"User {userName} logged in.");
+        break;
+    }
+    else
+    {
+        atempts--;
+        if (atempts > 0)
+        {
+            Console.WriteLine("Incorrect password. Try again.");
+        }
+    }
+}
+
+if (atempts == 0)
+{
+    Console.WriteLine($"User {userName} blocked!");
+}
