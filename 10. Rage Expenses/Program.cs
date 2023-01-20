384c869cde37c1ad723d3872ef2bf98ef5a7daed
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-01-20 23:46:46 +0200

    Add files via upload

---
+++ 10. Rage Expenses/Program.cs
@@ -0,0 +1,28 @@
+﻿int lostGames = int.Parse(Console.ReadLine());
+double headsetPrice = double.Parse(Console.ReadLine());
+double mousePrice = double.Parse(Console.ReadLine());
+double keyboardPrice = double.Parse(Console.ReadLine());
+double displayPrice = double.Parse(Console.ReadLine());
+
+double sumHeadsets = headsetPrice * (lostGames / 2);
+double sumMouses = mousePrice * (lostGames / 3);
+double sumKeyboards = keyboardPrice * (lostGames / 6);
+double sumDisplays = displayPrice * (lostGames / 12);
+
+double expenses = sumHeadsets + sumMouses + sumKeyboards + sumDisplays;
+
+Console.WriteLine($"Rage expenses: {expenses:f2} lv.");
+
+
+
+
+//int lostGames = int.Parse(Console.ReadLine());
+//double headsetsPrice = double.Parse(Console.ReadLine()) * (lostGames / 2);
+//double mousesPrice = double.Parse(Console.ReadLine()) * (lostGames / 3);
+//double keyboardsPrice = double.Parse(Console.ReadLine()) * (lostGames / 6);
+//double displaysPrice = double.Parse(Console.ReadLine()) * (lostGames / 12);
+
+
+//double expenses = headsetsPrice + mousesPrice + keyboardsPrice + displaysPrice;
+
+//Console.WriteLine($"Rage expenses: {expenses:f2} lv.");
