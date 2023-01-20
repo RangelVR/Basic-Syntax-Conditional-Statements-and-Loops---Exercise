a2301afff66dcf27479a49ed36e3335678b3dae1
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-01-20 23:08:28 +0200

    Add files via upload

---
+++ 09. Padawan Equipment/Program.cs
@@ -0,0 +1,26 @@
+﻿double money = double.Parse(Console.ReadLine());
+int students = int.Parse(Console.ReadLine());
+double saberPrice = double.Parse(Console.ReadLine());
+double robePrice = double.Parse(Console.ReadLine());
+double beltPrice = double.Parse(Console.ReadLine());
+
+double priceOfBelts = students * beltPrice;
+double priceOfSabers = Math.Ceiling(students * 1.1) * saberPrice;
+double priceOfRobes = students * robePrice;
+
+
+if (students >= 6)
+{
+    priceOfBelts -= (students / 6) * beltPrice;
+}
+
+double neededMoney = priceOfBelts + priceOfSabers + priceOfRobes;
+
+if (neededMoney <= money)
+{
+    Console.WriteLine($"The money is enough - it would cost {neededMoney:f2}lv.");
+}
+else
+{
+    Console.WriteLine($"John will need {(neededMoney - money):f2}lv more.");
+}
