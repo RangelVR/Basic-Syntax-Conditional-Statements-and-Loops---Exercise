4a7fd6752d9b84d7665a44d3e27f27eb2aff0dd2
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-01-21 12:58:10 +0200

    Add files via upload

---
+++ 11. Orders/Program.cs
@@ -0,0 +1,17 @@
+﻿int orders = int.Parse(Console.ReadLine());
+
+double totalPrice = 0;
+
+for (int i = 0; i < orders; i++)
+{
+    double capsulePrice = double.Parse(Console.ReadLine());
+    int days = int.Parse((Console.ReadLine()));
+    int capsules = int.Parse(Console.ReadLine());
+
+    double cofePrice = 0;
+    cofePrice = days * capsules * capsulePrice;
+    Console.WriteLine($"The price for the coffee is: ${cofePrice:f2}");
+    totalPrice += cofePrice;
+}
+
+Console.WriteLine($"Total: ${totalPrice:f2}");
